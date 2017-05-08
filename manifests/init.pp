0123a2e0bc5b8511b5edf1c032f0d07df23f21c9
Kwasi Appiah <kwasi@appiah.id.au>
2017-05-08 11:54:16 +1000

    Parse Ruby2.4 as a ruby version

--- manifests/init.pp
+++ manifests/init.pp
@@ -152,6 +152,12 @@ class ruby (
                 warning('Packages for Ruby 2.3 are not available from default repositories.')
               }
             }
+            /^2\.4(\..*)*$/:{
+              $real_ruby_package  = "${ruby::params::ruby_package}2.4"
+              if ! $suppress_warnings and versioncmp($::operatingsystemrelease, '16.04') < 0 {
+                warning('Packages for Ruby 2.4 are not available from default repositories.')
+              }
+            }
             default: {
               $real_ruby_package  = $ruby_package
             }
@@ -246,6 +252,12 @@ class ruby (
                 warning('No binary for Ruby 2.3.x available from default repositories')
               }
             }
+            /^2\.4(\..*)*$/:{
+              $real_default_bin  = "${ruby::params::ruby_bin_base}2.4"
+              if ! $suppress_warnings {
+                warning('No binary for Ruby 2.4.x available from default repositories')
+              }
+            }
             default: {
               fail('Unable to resolve default ruby binary')
             }
@@ -286,6 +298,12 @@ class ruby (
               $real_default_gem  = "${ruby::params::ruby_gem_base}2.3"
               if ! $suppress_warnings {
                 warning('No binary package for Ruby 2.3.x available from default repositories')
+              }
+            }
+            /^2\.4(\..*)*$/:{
+              $real_default_gem  = "${ruby::params::ruby_gem_base}2.4"
+              if ! $suppress_warnings {
+                warning('No binary package for Ruby 2.4.x available from default repositories')
               }
             }
             default: {
